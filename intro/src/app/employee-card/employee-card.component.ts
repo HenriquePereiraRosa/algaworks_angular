c596c2858e357731422eff30ca21cb2c8176a375
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-05-17 12:40:16 -0300

    9.16 Addind an Array of CSS Styles in components

--- intro/src/app/employee-card/employee-card.component.ts
+++ intro/src/app/employee-card/employee-card.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-employee-card',
   templateUrl: './employee-card.component.html',
-  styleUrls: ['./employee-card.component.css']
+  //styleUrls: ['./employee-card.component.css']
+  styles: [`
+    .card-body {
+      text-transform: uppercase;
+      color: dodgerblue;
+    }
+      `]
 })
 
 export class EmployeeCardComponent implements OnInit {
