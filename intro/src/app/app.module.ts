4c10caefc31ab49d1a05e0de1788d69852d6c734
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-05-24 16:00:15 -0300

    10.11 creating a navbar component

--- intro/src/app/app.module.ts
+++ intro/src/app/app.module.ts
@@ -10,9 +10,13 @@ import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
