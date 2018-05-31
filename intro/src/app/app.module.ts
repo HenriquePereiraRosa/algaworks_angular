f2aefa6c71f97a74c63f1f96b6ed6b4d6745765e
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-05-31 00:00:48 -0300

    Added lancamento-cadastro component

--- intro/src/app/app.module.ts
+++ intro/src/app/app.module.ts
@@ -13,12 +13,14 @@ import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
+    LancamentoCadastroComponent,
   ],
   imports: [
     BrowserModule,
