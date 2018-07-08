f1f1345a0284e9f38d9e57b3db263f9545abbffe
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2018-07-08 20:42:36 -0300

    Fixed envoironment.prod server name.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://angular-intro.herokuapp.com',
+  apiUrl: 'https://springbootintro.herokuapp.com/',
 
-  tokenWhitelistedDomains: [ new RegExp('https://angular-intro.herokuapp.com') ],
+  tokenWhitelistedDomains: [ new RegExp('https://springbootintro.herokuapp.com/') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
